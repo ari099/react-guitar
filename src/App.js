75a1fecfe0bc9f307945907e00d2ec5bea7a33b8
Ariel Lorenzo <ariellorenzo08@gmail.com>
2020-01-01 22:29:35 -0500

    Added some variables to main App component for simpler positioning of elements.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import guitarLogo from './guitar-logo.svg';
 import './App.css';
 
 function App() {
+  let lineX = 95;
+  let r = 5;
+  let topBridgeY = 10;
+  let bottomBridgeY = 300;
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={guitarLogo} className="App-logo" alt="logo" /> */}
         <svg className="App-guitar" height={310} width={350}>
           <circle className="App-guitar-soundhole" />
-          <circle className="App-guitar-bridgehole" r={5} cx={95} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={125} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={155} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={185} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={215} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={245} cy={10} />
-          <circle className="App-guitar-bridgehole" r={5} cx={95} cy={300} />
-          <circle className="App-guitar-bridgehole" r={5} cx={125} cy={300} />
-          <circle className="App-guitar-bridgehole" r={5} cx={155} cy={300} />
-          <circle className="App-guitar-bridgehole" r={5} cx={185} cy={300} />
-          <circle className="App-guitar-bridgehole" r={5} cx={215} cy={300} />
-          <circle className="App-guitar-bridgehole" r={5} cx={245} cy={300} />
-          <line className="App-guitar-string" x1={95} y1={10} x2={95} y2={300} />
-          <line className="App-guitar-string" x1={125} y1={10} x2={125} y2={300} />
-          <line className="App-guitar-string" x1={155} y1={10} x2={155} y2={300} />
-          <line className="App-guitar-string" x1={185} y1={10} x2={185} y2={300} />
-          <line className="App-guitar-string" x1={215} y1={10} x2={215} y2={300} />
-          <line className="App-guitar-string" x1={245} y1={10} x2={245} y2={300} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+30} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+60} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+90} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+120} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+150} cy={topBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX} cy={bottomBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+30} cy={bottomBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+60} cy={bottomBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+90} cy={bottomBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+120} cy={bottomBridgeY} />
+          <circle className="App-guitar-bridgehole" r={r} cx={lineX+150} cy={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX} y1={topBridgeY} x2={lineX} y2={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX+30} y1={topBridgeY} x2={lineX+30} y2={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX+60} y1={topBridgeY} x2={lineX+60} y2={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX+90} y1={topBridgeY} x2={lineX+90} y2={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX+120} y1={topBridgeY} x2={lineX+120} y2={bottomBridgeY} />
+          <line className="App-guitar-string" x1={lineX+150} y1={topBridgeY} x2={lineX+150} y2={bottomBridgeY} />
         </svg>
         <p className="App-guitar-chord-root">C</p>
       </header>
